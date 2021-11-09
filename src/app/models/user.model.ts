c91b9802bfa9090920a08a6fb9e3ca7be5aa4a95
bazev <evan.bazin@gmail.com>
2021-11-09 13:25:15 +0100

    maj

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,81 @@
+export class User {
+
+  private _id: number | null;
+  private _email: string;
+  private _first_name: string;
+  private _last_name: string;
+  private _password: string;
+
+  constructor(email: string, password:string, first_name: string, last_name: string, id?: number) {
+    if (typeof id === 'number') {
+      this._id = id;
+    } else {
+      this._id = null;
+    }
+    this._password = password;
+    this._email = email;
+    this._first_name = first_name;
+    this._last_name = last_name;
+  }
+
+
+  get id(): number | null {
+    return this._id;
+  }
+
+  set id(value: number | null) {
+    this._id = value;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  set email(value: string) {
+    this._email = value;
+  }
+
+  get first_name(): string {
+    return this._first_name;
+  }
+
+  set first_name(value: string) {
+    this._first_name = value;
+  }
+
+  get last_name(): string {
+    return this._last_name;
+  }
+
+  set last_name(value: string) {
+    this._last_name = value;
+  }
+
+
+  get password(): string {
+    return this._password;
+  }
+
+  set password(value: string) {
+    this._password = value;
+  }
+
+  static fromJson(userAsJson: any): User {
+    return new User(
+      userAsJson.email,
+      userAsJson.password,
+      userAsJson.first_name,
+      userAsJson.last_name,
+      userAsJson.id,
+    );
+  }
+
+  toJson(): any {
+    return {
+      _email: this.email,
+      _password:this.password,
+      _first_name: this.first_name,
+      _last_name: this.last_name,
+    };
+  }
+}
--- src/app/sercices/auth.service.ts
+++ src/app/sercices/auth.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -8,19 +8,23 @@ import {HttpClient} from "@angular/common/http";
 export class AuthService {
   token: BehaviorSubject<boolean>
 
+
+
   constructor(private http: HttpClient) {
     this.token = new BehaviorSubject<boolean>(false);
   }
 
   login(email: string, password: string): Promise<any> {
+    const options = {headers:new HttpHeaders       ({
+        'Access-Control-Allow-Origin':'*'
+      })}
     return this.http
-      .post('https://picom.herokuapp.com/ws/login/', {
-        email,
-        password,
-      })
+      .post('http://localhost:8080/ws/login/'+email+'/'+password,
+        {
+      }, options)
       .toPromise()
-      .then(() => {
-        this.token.next(true)
+      .then((resp:any) => {
+        this.token.next(resp)
       })
   }
 
